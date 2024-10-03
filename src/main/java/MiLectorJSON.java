e8f11d69e42e63ebfa13e4a5d393a6cda23d08df
davidenrique <davidenriquea@icloud.com>
2024-10-03 20:44:38 +0200

    Característica añadida: Escritura

--- src/main/java/MiLectorJSON.java
+++ src/main/java/MiLectorJSON.java
@@ -1,21 +1,37 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class MiLectorJSON {
 
     public static void main(String[] args) {
-        String filePath = "src/main/java/datosUsuarios.json"; // Cambia esto por la ruta real de tu archivo JSON
+        String filePath = "src/main/java/datosUsuarios.json";
+        String filePathModificado = "src/main/java/datosUsuarioModificado.json";
 
         try {
             String content = new String(Files.readAllBytes(Paths.get(filePath)));
             JSONObject objetoJSON = new JSONObject(content);
-
+            JSONArray habilidades  = objetoJSON.getJSONArray("habilidades");
             // Acceder al elemento "nombre" y mostrarlo:
             String nombre = objetoJSON.getString("nombre");
             System.out.println("El nombre en el JSON es: " + nombre);
 
+            System.out.println("Habilidades actuales: " + habilidades);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ingrese nueva habilidad de la persona" + nombre + ": ");
+            String nuevaHabilidad = scanner.nextLine();
+
+            habilidades.put(nuevaHabilidad);
+            System.out.println("Habilidad modificadas: " + habilidades);
+
+
+            Files.write(Paths.get(filePathModificado), objetoJSON.toString(4).getBytes());
+            System.out.println("Datos guardados en el fichero: " + filePathModificado);
         } catch (IOException e) {
             System.out.println("Error leyendo el archivo: " + e.getMessage());
         }
